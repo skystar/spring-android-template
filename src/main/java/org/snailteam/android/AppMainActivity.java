e1330361791b3bc98629be0278ede2c3de7a1f42
steve.zhijun.fan <xiaofancn@gmail.com>
2012-02-10 10:30:02 +0800

    Signed-off-by: steve.zhijun.fan <xiaofancn@gmail.com>

---
+++ src/main/java/org/snailteam/android/AppMainActivity.java
@@ -0,0 +1,16 @@
+package org.snailteam.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class AppMainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
+
