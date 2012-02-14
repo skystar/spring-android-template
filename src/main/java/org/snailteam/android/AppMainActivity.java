159c4a73f7b0211f80d94134e8a40f80cf7ed517
steve.zhijun.fan <xiaofancn@gmail.com>
2012-02-14 23:05:55 +0800

    Signed-off-by: steve.zhijun.fan <xiaofancn@gmail.com>

--- src/main/java/org/snailteam/android/AppMainActivity.java
+++ src/main/java/org/snailteam/android/AppMainActivity.java
@@ -1,35 +1,20 @@
 package org.snailteam.android;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.snailteam.android.adapter.ItemCellGridViewAdapter;
-import org.snailteam.android.model.CityDTO;
-import org.snailteam.android.model.Shop;
+import org.snailteam.android.adapter.ProductCellGridViewAdapter;
+import org.snailteam.android.adapter.ShopInfoGridViewAdapter;
+import org.snailteam.android.until.MenuStaut;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class AppMainActivity extends Activity {
 	public static String url = "http://192.168.0.103:8080/team/api";
-
+	MenuStaut currStatu = MenuStaut.MENU;
 	GridView gridView;
 
 	@Override
@@ -38,6 +23,24 @@ public class AppMainActivity extends Activity {
 		setContentView(R.layout.main);
 		gridView = (GridView) findViewById(R.id.gridView1);
 		gridView.setAdapter(new ItemCellGridViewAdapter(this));
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				if (currStatu == MenuStaut.MENU) {
+					currStatu = MenuStaut.PRODUCT;
+					gridView.setAdapter(new ProductCellGridViewAdapter(
+							AppMainActivity.this));
+				} else if (currStatu == MenuStaut.PRODUCT) {
+					currStatu = MenuStaut.SHOPINFO;
+					gridView.setAdapter(new ShopInfoGridViewAdapter(
+							AppMainActivity.this));
+				} else if (currStatu == MenuStaut.SHOPINFO) {
+					currStatu = MenuStaut.MENU;
+					gridView.setAdapter(new ItemCellGridViewAdapter(
+							AppMainActivity.this));
+				}
+			}
+		});
 	}
 
 }
---
+++ src/main/java/org/snailteam/android/until/MenuStaut.java
@@ -0,0 +1,5 @@
+package org.snailteam.android.until;
+
+public enum MenuStaut {
+	MENU, PRODUCT, SHOPINFO
+}
